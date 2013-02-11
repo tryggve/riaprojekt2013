4dbe418b88d0054e3c43c3b89ba11d7d9dfa6cb0
tryggve <tryggve@tryggve.nu>
2013-02-11 23:59:07 +0100

    Got a hello world printed. Using BB Model, View and HB Template. Also wired jqUI

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@
 requirejs.config({
     //Add a timestamp to the resources so it can't cache
     urlArgs: "bust=v1." + (new Date().getTime()),
-    //By default load any module IDs from js/lib
+    //By default load any module IDs from scripts/lib
     baseUrl: 'scripts/libs',
     //except, if the module ID starts with "app",
     //load it from the scripts/app directory. paths
@@ -13,37 +13,58 @@ requirejs.config({
         app: "../app",
         jquery: "jquery-1.9.1.min",
         ui: "jquery-ui-1.10.0.custom.min",
-        relational: "backbone-relational"
+        relational: "backbone-relational",
+        localstorage: "backbone-localstorage"
     },
     shim: {
         backbone: {
             //These script dependencies should be loaded before loading
             //backbone.js
-            deps: ["underscore", "jquery"],
+            deps: ["underscore", "jquery", "handlebars"],
             //Once loaded, use the global "Backbone" as the
             //module value.
             exports: "Backbone"
         },
-        underscore: {
-            exports: "_"
-        },
         relational: {
+            deps: ["backbone"]
+        },
+        localstorage: {
             deps: ["backbone"]
         },
         ui: {
             deps: ["jquery"]
         },
-        jquery: {
-            exports: "$"
+        handlebars: {
+            exports: "Handlebars"
         }
     }
 });
 
 // Start the main app logic.
-requirejs(["backbone", "ui"], function(Backbone) {
+requirejs(["backbone", "handlebars", "ui"], function(Backbone, Handlebars) {
     _.forEach([1,2,3], function(each) {
         console.log(each);
     });
-    var hej = Backbone.Model.extend({});
+    var Hej = Backbone.Model.extend({
+        greeting: function() { return this.get("greeting"); }
+    });
+    var Template = Handlebars.compile("<div>{{greeting}}</div>");
+    var HejView = Backbone.View.extend({
+        el: "#container",
+        template: Template,
+        initialize: function() {
+            this.render();
+        },
+        render: function() {
+            this.$el.html(this.template(this));
+            console.log(this);
+            return this;
+        },
+        greeting: function() { return this.model.greeting(); }
+    });
+
+    var hej = new Hej({greeting: "Hej världen"});
+    var hejView = new HejView({model: hej});
+
     $("body").append($("<div>").html("Hej").draggable());
 });
